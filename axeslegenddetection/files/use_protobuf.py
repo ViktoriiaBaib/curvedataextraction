9adc5e22d82b58bbb3ea8097f720d4a13983b952
Viktoriia Baibakova <35492311+ViktoriiaBaib@users.noreply.github.com>
2022-01-24 17:53:11 -0800

    Delete use_protobuf.py

--- axeslegenddetection/files/use_protobuf.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-args = sys.argv
-directory = args[1]
-protoc_path = args[2]
-for file in os.listdir(directory):
-	if file.endswith(".proto"):
-		os.system(protoc_path+" "+directory+"/"+file+" --python_out=.")
